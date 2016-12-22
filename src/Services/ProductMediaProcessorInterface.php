0c7f9d582f8db3cd4a62ca80f75e8e3495f88bfb
wagnert <wagner_tim78@hotmail.com>
2016-12-22 19:19:24 +0100

    MediaSubject now extends AbstractProductSubject + add Robo.li composer dependeny + task configuration + ProductMediaProcessorInterface now extends ProductProcessorInterface

--- src/Services/ProductMediaProcessorInterface.php
+++ src/Services/ProductMediaProcessorInterface.php
@@ -20,6 +20,8 @@
 
 namespace TechDivision\Import\Product\Media\Services;
 
+use TechDivision\Import\Product\Services\ProductProcessorInterface;
+
 /**
  * A SLSB providing methods to load product data using a PDO connection.
  *
@@ -29,50 +31,8 @@ namespace TechDivision\Import\Product\Media\Services;
  * @link      https://github.com/techdivision/import-product-media
  * @link      http://www.techdivision.com
  */
-interface ProductMediaProcessorInterface
+interface ProductMediaProcessorInterface extends ProductProcessorInterface
 {
-
-    /**
-     * Return's the connection.
-     *
-     * @return \PDO The connection instance
-     */
-    public function getConnection();
-
-    /**
-     * Turns off autocommit mode. While autocommit mode is turned off, changes made to the database via the PDO
-     * object instance are not committed until you end the transaction by calling ProductProcessor::commit().
-     * Calling ProductProcessor::rollBack() will roll back all changes to the database and return the connection
-     * to autocommit mode.
-     *
-     * @return boolean Returns TRUE on success or FALSE on failure
-     * @link http://php.net/manual/en/pdo.begintransaction.php
-     */
-    public function beginTransaction();
-
-    /**
-     * Commits a transaction, returning the database connection to autocommit mode until the next call to
-     * ProductProcessor::beginTransaction() starts a new transaction.
-     *
-     * @return boolean Returns TRUE on success or FALSE on failure
-     * @link http://php.net/manual/en/pdo.commit.php
-     */
-    public function commit();
-
-    /**
-     * Rolls back the current transaction, as initiated by ProductProcessor::beginTransaction().
-     *
-     * If the database was set to autocommit mode, this function will restore autocommit mode after it has
-     * rolled back the transaction.
-     *
-     * Some databases, including MySQL, automatically issue an implicit COMMIT when a database definition
-     * language (DDL) statement such as DROP TABLE or CREATE TABLE is issued within a transaction. The implicit
-     * COMMIT will prevent you from rolling back any other changes within the transaction boundary.
-     *
-     * @return boolean Returns TRUE on success or FALSE on failure
-     * @link http://php.net/manual/en/pdo.rollback.php
-     */
-    public function rollBack();
 
     /**
      * Return's the action with the product media gallery CRUD methods.
