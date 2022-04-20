effa24050fab3d9d2ada507887fb8b3f9a9236a1
Kenza Yamlahi <k.yamlahi@techdivision.com>
2022-04-20 16:17:11 +0200

    add dummy unit test

---
+++ tests/unit/Observer/ImportProductMediaTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * PHP version 7
+ *
+ * @author    MET <met@techdivision.com>
+ * @copyright 2022 TechDivision GmbH <info@techdivision.com>
+ * @license   https://opensource.org/licenses/MIT
+ * @link      https://github.com/techdivision/import-ee
+ * @link      http://www.techdivision.com
+ */
+
+namespace TechDivision\Import\Product\Link\Observer;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Dummy Test for workflow
+ *
+ * @author    MET <met@techdivision.com>
+ * @copyright 2022 TechDivision GmbH <info@techdivision.com>
+ * @license   https://opensource.org/licenses/MIT
+ * @link      https://github.com/techdivision/import-ee
+ * @link      http://www.techdivision.com
+ */
+class ImportProductMediaTest extends TestCase
+{
+
+    /**
+     * Dummy test implementation.
+     *
+     * @return void
+     */
+    public function testDummy()
+    {
+        $this->markTestSkipped('Still to implement');
+    }
+}
